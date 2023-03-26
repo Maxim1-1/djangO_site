9e0c5525f4d900a4038c7c77fdb8acf4cca3375a
Maxim1-1 <kick52@yadnex.ru>
2023-03-26 23:06:40 +0300

    add img on post from admin

--- mega_site/settings/urls.py
+++ mega_site/settings/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from blog.views import main_page, blog_page
-
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('blog/', include('blog.urls')),
@@ -28,3 +30,5 @@ urlpatterns = [
     path('blog', include('blog.urls')),
 
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

bd1c820fa41d6f2c8b349fdc8c57ccc6cc2bec28
Maxim1-1 <kick52@yadnex.ru>
2023-03-26 20:15:24 +0300

    add slug in endpoint

--- mega_site/blog/models.py
+++ mega_site/blog/models.py
@@ -7,11 +7,12 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=300)
-    slug = models.SlugField(max_length=300,unique=True,db_index=True)
+    img = models.ImageField(upload_to='post/img')
+    slug = models.SlugField(max_length=300, unique=True, db_index=True)
 
     def __str__(self):
         return self.title
 
-
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post': self.slug})
+        return reverse('post', kwargs={'post_slug': self.slug})
+
